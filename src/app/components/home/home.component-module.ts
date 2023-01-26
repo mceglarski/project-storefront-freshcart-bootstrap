114858594455c51dbcd22a5dfa5a13acb2d47bb8
Marcin Ceglarski <marcin.ceglarski0@gmail.com>
2023-01-26 22:06:07 +0100

    shop-by-category-to-home: added categories on home component

--- src/app/components/home/home.component-module.ts
+++ src/app/components/home/home.component-module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   declarations: [HomeComponent],
   providers: [],
-  exports: [HomeComponent]
+  exports: [HomeComponent],
 })
-export class HomeComponentModule {
-}
+export class HomeComponentModule {}
